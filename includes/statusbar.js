bec23ac6a0c289d84ab2ea4673451c8b2d346ad2
Joel Spadin <joelspadin@gmail.com>
2013-02-17 03:57:22 -0600

    Frames stuff

--- includes/statusbar.js
+++ includes/statusbar.js
@@ -20,6 +20,7 @@ var expandTimeoutId = null;
 var removeTimeoutId = null;
 var currentTarget = null;
 var isExpanded = false;
+var parentOrigin = null;
 /** Initializes the statusbar */
 function init() {
     EXPAND_TIMEOUT = JSON.parse(widget.preferences['expand']);
@@ -44,22 +45,32 @@ function init() {
         style = document.createElement('style');
         style.appendChild(document.createTextNode(fr.result));
         document.head.appendChild(style);
-        document.body.addEventListener('mouseover', show, false);
+        document.body.addEventListener('mouseover', mouseover, false);
     };
     fr.readAsText(stylesheet);
+}
+function initFrame() {
+    parentOrigin = window.parent.location.protocol + '//' + window.parent.location.host;
+    document.body.addEventListener('mouseover', mouseoverFrame, false);
+}
+function findAnchor(target) {
+    var t = MAX_TRAVERSE;
+    while(t > 0 && target && target.nodeName !== 'A' && target.nodeName !== 'AREA') {
+        target = target.parentElement;
+        --t;
+    }
+    if(!target || !(target).href) {
+        return null;
+    }
+    return target;
 }
 /**
 * Displays the statusbar
 * @param e {MouseEvent} the mouseover event for any element
 */
-function show(e) {
-    var target = e.target;
-    var t = MAX_TRAVERSE;
-    while(t > 0 && target && target.nodeName !== 'A' && target.nodeName !== 'AREA') {
-        target = target.parentNode;
-        --t;
-    }
-    if(!target || !target.href) {
+function mouseover(e) {
+    var target = findAnchor(e.target);
+    if(!target) {
         return;
     }
     clearTimeout(hideTimeoutId);
@@ -67,38 +78,13 @@ function show(e) {
     clearTimeout(removeTimeoutId);
     if(currentTarget !== target) {
         currentTarget = target;
-        var url = target.href;
-        var protocol = target.protocol.replace(':', '');
-        var domain = target.hostname;
-        var subdomain = '';
-        // Find the subdomain (if any)
-        var domainParts = domain.split('.');
-        if(domainParts.length > 2) {
-            subdomain = domainParts.shift() + '.';
-            domain = domainParts.join('.');
-        }
-        // number of characters between protocol and subdomain
-        var sepLength = 1;
-        while(url.charAt(protocol.length + sepLength) === '/') {
-            sepLength += 1;
-        }
-        protocolText.textContent = protocol;
-        subdomainText.textContent = subdomain;
-        domainText.textContent = domain;
-        pathText.textContent = decodeURIComponent(url.substr(protocol.length + sepLength + subdomain.length + domain.length).trim());
-        statusbar.className = protocol;
-        statusbar.style.display = 'block';
-        statusbar.style.maxWidth = isExpanded ? '100%' : Math.min(MAX_WIDTH, document.documentElement.clientWidth) + 'px';
-        // If the mouse is over where the statusbar should appear, don't show the statusbar
-        var box = statusbar.getBoundingClientRect();
-        if(e.clientX > box.left && e.clientX < box.right && e.clientY > box.top && e.clientY < box.bottom) {
-            hideMouseover();
-            return;
-        }
-        // set the statusbar to fade in
-        setTimeout(function () {
-            statusbar.style.opacity = '1';
-        }, 0);
+        show({
+            href: target.href,
+            protocol: target.protocol,
+            hostname: target.hostname,
+            clientX: e.clientX,
+            clientY: e.clientY
+        });
     }
     // set the statusbar to expand after a moment
     expandTimeoutId = setTimeout(function () {
@@ -111,6 +97,66 @@ function show(e) {
         target.removeEventListener('mouseout', onMouseOut, false);
     }
     target.addEventListener('mouseout', onMouseOut, false);
+}
+function mouseoverFrame(e) {
+    var target = findAnchor(e.target);
+    if(!target) {
+        return;
+    }
+    if(currentTarget !== target) {
+        currentTarget = target;
+        window.parent.postMessage({
+            operaStatusBarEvent: 'show',
+            linkdata: {
+                href: target.href,
+                protocol: target.protocol,
+                hostname: target.hostname,
+                clientX: e.clientX,
+                clientY: e.clientY
+            }
+        }, parentOrigin);
+    }
+    function onMouseOut(e) {
+        window.parent.postMessage({
+            operaStatusBarEvent: 'hide'
+        }, parentOrigin);
+        target.removeEventListener('mouseout', onMouseOut, false);
+    }
+    target.addEventListener('mouseout', onMouseOut, false);
+}
+function show(linkdata) {
+    var url = linkdata.href;
+    var protocol = linkdata.protocol.replace(':', '');
+    var domain = linkdata.hostname;
+    var subdomain = '';
+    // Find the subdomain (if any)
+    var domainParts = domain.split('.');
+    if(domainParts.length > 2) {
+        subdomain = domainParts.shift() + '.';
+        domain = domainParts.join('.');
+    }
+    // number of characters between protocol and subdomain
+    var sepLength = 1;
+    while(url.charAt(protocol.length + sepLength) === '/') {
+        sepLength += 1;
+    }
+    protocolText.textContent = protocol;
+    subdomainText.textContent = subdomain;
+    domainText.textContent = domain;
+    pathText.textContent = decodeURIComponent(url.substr(protocol.length + sepLength + subdomain.length + domain.length).trim());
+    statusbar.className = protocol;
+    statusbar.style.display = 'block';
+    statusbar.style.maxWidth = isExpanded ? '100%' : Math.min(MAX_WIDTH, document.documentElement.clientWidth) + 'px';
+    // If the mouse is over where the statusbar should appear, don't show the statusbar
+    var box = statusbar.getBoundingClientRect();
+    if(linkdata.clientX > box.left && linkdata.clientX < box.right && linkdata.clientY > box.top && linkdata.clientY < box.bottom) {
+        hideMouseover();
+        return;
+    }
+    // set the statusbar to fade in
+    setTimeout(function () {
+        statusbar.style.opacity = '1';
+    }, 0);
 }
 /** Hides the statusbar by fading it out */
 function hide() {
@@ -137,14 +183,33 @@ function hideMouseover() {
     statusbar.style.opacity = '0';
     statusbar.style.display = 'none';
 }
+function onMessage(e) {
+    var event;
+    if(typeof e.data === 'object' && (event = e.data.operaStatusbar)) {
+        if(event === 'show') {
+            show(e.data.linkdata);
+        } else if(event === 'hide') {
+            hide();
+        }
+    }
+}
 function onLoad() {
     // Frames are hard. Ignore them for now
-    if(window === window.top) {
+    if(window === window.top || window.document.body instanceof (window).HTMLFrameSetElement) {
+        console.log('top');
         init();
+        window.addEventListener('message', onMessage, false);
         // Kill the statusbar if the extension gets disabled
-        opera.extension.addEventListener('disconnection', function () {
-            document.body.removeEventListener('mouseover', show, false);
+        opera.extension.addEventListener('disconnect', function () {
+            document.body.removeEventListener('mouseover', mouseover, false);
             hide();
+        }, false);
+    } else {
+        console.log(document.body);
+        console.log('frame');
+        initFrame();
+        opera.extension.addEventListener('disconnect', function () {
+            document.body.removeEventListener('mouseover', mouseoverFrame, false);
         }, false);
     }
 }
